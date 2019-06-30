bcb539007b91127933ce2e9cbf801bb251ce2254
jc6458 <843078247@qq.com>
2019-06-30 23:43:55 +0800

    add table 2

--- src/views/SysManage/LogonRecord.vue
+++ src/views/SysManage/LogonRecord.vue
@@ -1,15 +1,77 @@
 <template>
-    <div>test2
-        <el-checkbox v-model="checked">备选项</el-checkbox>
+    <div class="app-containei">
+        <div class="filter-container">
+            <el-table
+                :data="tableData"
+                border
+                style="width: 100%">
+                <el-table-column
+                prop="date"
+                label="日期"
+                width="180">
+                </el-table-column>
+                <el-table-column
+                prop="name"
+                label="姓名"
+                width="180">
+                </el-table-column>
+                <el-table-column
+                prop="address"
+                label="地址">
+                </el-table-column>
+            </el-table>
+            <div class="block" style="margin: 20px 0;">
+                <el-pagination
+                    background
+                    @size-change="handleSizeChange"
+                    @current-change="handleCurrentChange"
+                    :current-page="currentPage4"
+                    :page-sizes="[10, 25, 50, 100]"
+                    :page-size="10"
+                    layout="total, sizes, prev, pager, next, jumper"
+                    :total="4">
+                </el-pagination>
+            </div>
+        </div>
     </div>
 </template>
 <script>
 export default {
     data() {
         return {
-            checked: false
+            tableData: [{
+                date: '2016-05-02',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
+            }, {
+                date: '2016-05-04',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1517 弄'
+            }, {
+                date: '2016-05-01',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1519 弄'
+            }, {
+                date: '2016-05-03',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1516 弄'
+            }],
+            currentPage4: 4
         };
     }
 };
 </script>
+<style lang="scss">
+// .app-container {
+//     padding: 20px;
+// }
+// .filter-container {
+//   padding-bottom: 10px;
 
+//   .filter-item {
+//     display: inline-block;
+//     vertical-align: middle;
+//     margin-bottom: 10px;
+//   }
+// }
+</style>
--- src/views/SysManage/UserList.vue
+++ src/views/SysManage/UserList.vue
@@ -19,7 +19,8 @@
                 </el-table-column>
                 <el-table-column
                 align="right">
-                <template slot="header" slot-scope="scope">
+                <!-- <template slot="header" slot-scope="scope"> -->
+                <template slot="header">
                     <el-input
                     v-model="search"
                     size="mini"
@@ -49,48 +50,48 @@
                 </el-pagination>
             </div>
         </div>
-        
     </div>
 </template>
 <script>
 export default {
+    name: 'UserList',
     data() {
         return {
             tableData: [{
-            date: '2016-05-02',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1518 弄'
+                date: '2016-05-02',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1518 弄'
             }, {
-            date: '2016-05-04',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1517 弄'
+                date: '2016-05-04',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1517 弄'
             }, {
-            date: '2016-05-01',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄'
+                date: '2016-05-01',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1519 弄'
             }, {
-            date: '2016-05-03',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1516 弄'
+                date: '2016-05-03',
+                name: '王小虎',
+                address: '上海市普陀区金沙江路 1516 弄'
             }],
             search: '',
             input1: '',
             currentPage4: 4
-        }
+        };
     },
     methods: {
-      handleEdit(index, row) {
-        console.log(index, row);
-      },
-      handleDelete(index, row) {
-        console.log(index, row);
-      },
-      handleSizeChange(val) {
-        console.log(`每页 ${val} 条`);
-      },
-      handleCurrentChange(val) {
-        console.log(`当前页: ${val}`);
-      }
+        handleEdit(index, row) {
+            console.log(index, row);
+        },
+        handleDelete(index, row) {
+            console.log(index, row);
+        },
+        handleSizeChange(val) {
+            console.log(`每页 ${val} 条`);
+        },
+        handleCurrentChange(val) {
+            console.log(`当前页: ${val}`);
+        }
     }
 };
 </script>
